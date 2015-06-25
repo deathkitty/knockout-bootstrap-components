4dfce196982561bae6a02f590b9cd1541851cee1
Johan van der Merwe <johanvdm@compuscan.co.za>
2015-06-25 13:06:26 +0200

    Removed some testing logs.

--- src/knockout-bootstrap-code.js
+++ src/knockout-bootstrap-code.js
@@ -10,11 +10,7 @@ $(function(){
             self.tagId = ko.observable(kobs.guid());
             
             self.replaceTags = function(data){
-                console.log(kobs.guid());
-                
                 var str = "";
-                
-                console.log(data);
                 
                 for(var index in data){
                     if(data[index].outerHTML){
@@ -26,14 +22,11 @@ $(function(){
 
                 str = kobs.escapeHTML(str);
                 
-                console.log(str);
-                
                 document.getElementById(self.tagId()).innerHTML = str;
 
                 $('pre #' + self.tagId()).each(function(i, block) {
                     hljs.highlightBlock(block);
                 });
-                //return str;
             };
         },
         template: kobsTemplates["templates/knockout-bootstrap-code.html"]
